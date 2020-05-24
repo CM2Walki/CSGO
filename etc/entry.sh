438c171e0195378632e23781ef64ea58000edaaf
CM2Walki <walentinlamonos@googlemail.com>
2020-05-25 00:15:56 +0200

    Additional files

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+${STEAMCMDDIR}/steamcmd.sh +login anonymous \
+				+force_install_dir ${STEAMAPPDIR} \
+				+app_update ${STEAMAPPID} \
+				+quit
 
-${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} +quit
-
-${STEAMAPPDIR}/srcds_run -game csgo -console -autoupdate -steam_dir ${STEAMCMDDIR} -steamcmd_script ${STEAMAPPDIR}/csgo_update.txt -usercon +fps_max $SRCDS_FPSMAX \
-			-tickrate $SRCDS_TICKRATE -port $SRCDS_PORT +tv_port $SRCDS_TV_PORT +clientport $SRCDS_CLIENT_PORT -maxplayers_override $SRCDS_MAXPLAYERS +game_type $SRCDS_GAMETYPE +game_mode $SRCDS_GAMEMODE \
-			+mapgroup $SRCDS_MAPGROUP +map $SRCDS_STARTMAP +sv_setsteamaccount $SRCDS_TOKEN +rcon_password $SRCDS_RCONPW +sv_password $SRCDS_PW +sv_region $SRCDS_REGION
+${STEAMAPPDIR}/srcds_run" -game csgo 
+			-console \
+			-autoupdate \
+			-steam_dir ${STEAMCMDDIR} \
+			-steamcmd_script ${STEAMAPPDIR}/csgo_update.txt \
+			-usercon \
+			+fps_max $SRCDS_FPSMAX \
+			-tickrate $SRCDS_TICKRATE \
+			-port $SRCDS_PORT \
+			+tv_port $SRCDS_TV_PORT \
+			+clientport $SRCDS_CLIENT_PORT \
+			-maxplayers_override $SRCDS_MAXPLAYERS \
+			+game_type $SRCDS_GAMETYPE \
+			+game_mode $SRCDS_GAMEMODE \
+			+mapgroup $SRCDS_MAPGROUP \
+			+map $SRCDS_STARTMAP \
+			+sv_setsteamaccount $SRCDS_TOKEN \
+			+rcon_password $SRCDS_RCONPW \
+			+sv_password $SRCDS_PW \
+			+sv_region $SRCDS_REGION \
+			+net_public_adr $SRCDS_NET_PUBLIC_ADDRESS \
+			-ip $SRCDS_IP \
+			+host_workshop_collection $SRCDS_HOST_WORKSHOP_COLLECTION \
+			+workshop_start_map $SRCDS_WORKSHOP_START_MAP \
+			-authkey $SRCDS_WORKSHOP_AUTHKEY
