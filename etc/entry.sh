36208c69d733b8c2854792346a8f38e1fb9dfb7d
Walentin <walentinlamonos@googlemail.com>
2021-12-12 23:24:28 +0100

    Readds support to set rcon and server password via environment variables (#63)
    * Passwords can now again be set via environment variables #62
    * Added more comments explaining overwriting launch parameters

--- etc/entry.sh
+++ etc/entry.sh
@@ -27,12 +27,17 @@ if [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg" ]; then
 	sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
 fi
 
-# Check if autoexec file exists. Easier to copy config between servers. (I was migrating when I wrote this)
-# Passing them directly to srcds_run to ignores values set in autoexec.cfg.
+# Believe it or not, if you don't do this srcds_run shits itself
+cd "${STEAMAPPDIR}"
+
+# Check if autoexec file exists
+# Passing arguments directly to srcds_run, ignores values set in autoexec.cfg
 autoexec_file="${STEAMAPPDIR}/${STEAMAPP}/cfg/autoexec.cfg"
+
 # Overwritable arguments
 ow_args=""
 
+# If you need to overwrite a specific launch argument, add it to this loop and drop it from the subsequent srcds_run call
 if [ -f "$autoexec_file" ]; then
         # TAB delimited name    default
         # HERE doc to not add extra file
@@ -45,32 +50,54 @@ if [ -f "$autoexec_file" ]; then
 sv_password	+sv_password "${SRCDS_PW}"
 rcon_password	+rcon_password "${SRCDS_RCONPW}"
 EOM
-
+	# if autoexec is present, drop overwritten arguments here (example: SRCDS_PW & SRCDS_RCONPW)
+	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
+				-steam_dir "${STEAMCMDDIR}" \
+				-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
+				-usercon \
+				+fps_max "${SRCDS_FPSMAX}" \
+				-tickrate "${SRCDS_TICKRATE}" \
+				-port "${SRCDS_PORT}" \
+				+tv_port "${SRCDS_TV_PORT}" \
+				+clientport "${SRCDS_CLIENT_PORT}" \
+				-maxplayers_override "${SRCDS_MAXPLAYERS}" \
+				+game_type "${SRCDS_GAMETYPE}" \
+				+game_mode "${SRCDS_GAMEMODE}" \
+				+mapgroup "${SRCDS_MAPGROUP}" \
+				+map "${SRCDS_STARTMAP}" \
+				+sv_setsteamaccount "${SRCDS_TOKEN}" \
+				+sv_region "${SRCDS_REGION}" \
+				+net_public_adr "${SRCDS_NET_PUBLIC_ADDRESS}" \
+				-ip "${SRCDS_IP}" \
+				+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
+				+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
+				-authkey "${SRCDS_WORKSHOP_AUTHKEY}" \
+				"${ow_args}" \
+				"${ADDITIONAL_ARGS}"
+else
+	# If no autoexec is present, use all parameters
+	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
+				-steam_dir "${STEAMCMDDIR}" \
+				-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
+				-usercon \
+				+fps_max "${SRCDS_FPSMAX}" \
+				-tickrate "${SRCDS_TICKRATE}" \
+				-port "${SRCDS_PORT}" \
+				+tv_port "${SRCDS_TV_PORT}" \
+				+clientport "${SRCDS_CLIENT_PORT}" \
+				-maxplayers_override "${SRCDS_MAXPLAYERS}" \
+				+game_type "${SRCDS_GAMETYPE}" \
+				+game_mode "${SRCDS_GAMEMODE}" \
+				+mapgroup "${SRCDS_MAPGROUP}" \
+				+map "${SRCDS_STARTMAP}" \
+				+sv_setsteamaccount "${SRCDS_TOKEN}" \
+				+rcon_password "${SRCDS_RCONPW}" \
+				+sv_password "${SRCDS_PW}" \
+				+sv_region "${SRCDS_REGION}" \
+				+net_public_adr "${SRCDS_NET_PUBLIC_ADDRESS}" \
+				-ip "${SRCDS_IP}" \
+				+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
+				+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
+				-authkey "${SRCDS_WORKSHOP_AUTHKEY}" \
+				"${ADDITIONAL_ARGS}"
 fi
-
-# Believe it or not, if you don't do this srcds_run shits itself
-cd "${STEAMAPPDIR}"
-
-bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
-			-steam_dir "${STEAMCMDDIR}" \
-			-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
-			-usercon \
-			+fps_max "${SRCDS_FPSMAX}" \
-			-tickrate "${SRCDS_TICKRATE}" \
-			-port "${SRCDS_PORT}" \
-			+tv_port "${SRCDS_TV_PORT}" \
-			+clientport "${SRCDS_CLIENT_PORT}" \
-			-maxplayers_override "${SRCDS_MAXPLAYERS}" \
-			+game_type "${SRCDS_GAMETYPE}" \
-			+game_mode "${SRCDS_GAMEMODE}" \
-			+mapgroup "${SRCDS_MAPGROUP}" \
-			+map "${SRCDS_STARTMAP}" \
-			+sv_setsteamaccount "${SRCDS_TOKEN}" \
-			+sv_region "${SRCDS_REGION}" \
-			+net_public_adr "${SRCDS_NET_PUBLIC_ADDRESS}" \
-			-ip "${SRCDS_IP}" \
-			+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
-			+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
-			-authkey "${SRCDS_WORKSHOP_AUTHKEY}" \
-			"${ow_args}" \
-			"${ADDITIONAL_ARGS}"
